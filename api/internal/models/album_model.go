85caf9e5c595ea3f7ec7b36b2f22585e464a9dc4
Maik Mettenheimer <maik.mettenheimer@me.com>
2025-10-25 10:25:48 +0200

    docs:  gorm have problems with cascading deletes in many2many relations

--- api/internal/models/album_model.go
+++ api/internal/models/album_model.go
@@ -31,3 +31,17 @@ type AlbumMedia struct {
 	Album   Album `gorm:"foreignKey:AlbumID;constraint:OnDelete:CASCADE;"`
 	Media   Media `gorm:"foreignKey:MediaID;constraint:OnDelete:CASCADE;"`
 }
+
+/*
+Note: gorm have problems with cascading deletes in many2many relations.
+To ensure that deleting an album also removes its associations in the album_media table,
+we need to manually define the AlbumMedia model and set up the foreign key constraints with ON DELETE CASCADE.
+The following SQL commands should be run in the database to update the foreign key constraint:
+
+```sql
+ALTER TABLE album_media DROP FOREIGN KEY fk_albums_album_media;
+ALTER TABLE album_media
+ADD CONSTRAINT fk_albums_album_media
+FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE;
+```
+*/
